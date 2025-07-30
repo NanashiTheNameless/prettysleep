f4ffadc4d53855992b20eb15f7182f91991ef52e
Nanashi <53353250+NanashiTheNameless@users.noreply.github.com>
2025-07-30 02:55:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,10 +8,10 @@
 # Installation target directory
 DIR="$HOME/.local/bin"
 
-# Parse flags (only --agree is recognized)
+# Parse flags (only --agree or --accept are recognized)
 AGREE_FLAG=0
 for __arg in "$@"; do
-  if [[ "$__arg" == "--agree" ]]; then
+  if [[ "$__arg" == "--agree" || "$__arg" == "--accept" ]]; then
     AGREE_FLAG=1
   fi
 done
